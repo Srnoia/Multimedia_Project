156d0a9bb6f267d6abb5c63facacccd26d1b3038
TomNaN <tomna.git@gmail.com>
2015-09-09 18:26:00 +0300

    onrail movement now working
    Also added an fps counter for performance debugging

--- Hero.js
+++ Hero.js
@@ -2,8 +2,9 @@ function Hero(x,y){
   this.x = x;
   this.y = y;
   this.speed = 5;
-  this.dir = 0;
-  this.movement = [];
+  this.dir = 1;
+  this.stopped = true;
+  this.movement = null;
   this.rail = null;
   this.hitBox = {top:this.y,left:this.x,bottom:this.y+spriteHeight,right:this.x+spriteWidth};
   this.hitBox.centerX = (this.hitBox.left+this.hitBox.right)/2;
@@ -13,10 +14,12 @@ Hero.prototype.draw = function(){
   ctx.drawImage(spriteSheet, this.dir*spriteWidth, 2*spriteHeight, spriteWidth, spriteHeight, this.x, this.y, spriteWidth, spriteHeight);
 }
 Hero.prototype.move = function(){
-  this.dir==1?this.x-=this.speed:null;
-  this.dir==2?this.x+=this.speed:null;
-  this.dir==3?this.y+=this.speed:null;
-  this.dir==4?this.y-=this.speed:null;
+  if(!this.stopped){
+    this.dir==1?this.x-=this.speed:null;
+    this.dir==2?this.x+=this.speed:null;
+    this.dir==3?this.y+=this.speed:null;
+    this.dir==4?this.y-=this.speed:null;
+  }
   /*moveObj.up?(function(){this.y-=this.speed;this.dir=4;}).call(this,null):null;
   moveObj.left?(function(){this.x-=this.speed;this.dir=1;}).call(this,null):null;
   moveObj.right?(function(){this.x+=this.speed;this.dir=2;}).call(this,null):null;
