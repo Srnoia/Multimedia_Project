7ce865b9f6f5bad2c9c08a9aaba6212bd4bb2901
TomNaN <tomna.git@gmail.com>
2015-09-17 08:20:40 +0300

    Updated sprite proportions

--- wall.js
+++ wall.js
@@ -1,12 +1,13 @@
-function Wall(x,y,type){
+function Wall(x,y,typeX,typeY){
   this.x = x;
   this.y = y;
-  this.type = type;
+  this.typeX = typeX?typeX:0;
+  this.typeY = typeY?typeY:3;
   //this.hitBox = !this.type?{top:this.y,left:this.x+15,bottom:this.y+spriteHeight,right:this.x+spriteWidth-15}:{top:this.y+15,left:this.x,bottom:this.y+spriteHeight-15,right:this.x+spriteWidth};
 }
 Wall.prototype.draw = function(x){
   this.x = x;
-  ctx.drawImage(spriteSheet, this.type*spriteScreenWidth, 3*spriteScreenHeight, spriteScreenWidth, spriteScreenHeight, this.x, this.y, spriteWidth, spriteHeight);
+  ctx.drawImage(spriteSheet, this.typeX*spriteScreenWidth, this.typeY*spriteScreenHeight, spriteScreenWidth, spriteScreenHeight, this.x, this.y, spriteWidth, spriteHeight);
 }
 Wall.prototype.collision = function(entity){
   return true;
