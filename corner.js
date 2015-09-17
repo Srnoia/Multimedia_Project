baf71327b0098a568a1f08124ff3d0296f47e8d5
TomNaN <tomna.git@gmail.com>
2015-09-17 08:19:34 +0300

    Updated sprite proportions

--- corner.js
+++ corner.js
@@ -5,7 +5,7 @@ function Corner(x,y,type){
 }
 Corner.prototype.draw = function(x){
   this.x  = x;
-  ctx.drawImage(spriteSheet, this.type*spriteWidth, 4*spriteHeight, spriteWidth, spriteHeight, this.x, this.y, spriteWidth, spriteHeight);
+  ctx.drawImage(spriteSheet, this.type*spriteScreenWidth, 4*spriteScreenHeight, spriteScreenWidth, spriteScreenHeight, this.x, this.y, spriteWidth, spriteHeight);
 }
 Corner.prototype.collision = function(entity){
   return true;
